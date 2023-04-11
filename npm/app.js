37ca5b4b2944d079d1989d99a00e40140980ddea
Sanij Shrestha <saniz.sth123@gmail.com>
2023-04-11 12:14:56 -0400

    package.json and gitignore

---
+++ npm/app.js
@@ -0,0 +1,18 @@
+//checking npm version
+// npm --v
+// installing in a particular project
+// npm i <packagenme>
+
+//installing globally 
+// npm i -g <packagename>
+// sudo i -g <packagename> (for mac)
+
+// package.json -- stores important stuff about the package/project
+// installing package.json
+
+//npm init(step by step, enter to skip)
+// npm init -y(everything default)
+
+
+// why do we need package.json? to provide information regarding the project. 
+// you can find information regarding the dependencies, versions and other important stuff
